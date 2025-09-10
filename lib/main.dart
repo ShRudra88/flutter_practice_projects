3898d6e622997da76c3ec6f15622542727a4ec9e
ShRudra88 <salhawathosenrudra503@gmail.com>
2025-09-10 10:46:11 +0600

    Refactor: Simplify main and implement PageView
    This commit refactors the main application structure and introduces a `PageView` widget.
    
    Key changes:
    - Simplified `main.dart` by removing the `MyApp` and `MyHomePage` widgets.
    - Introduced `HomePage` as a `StatefulWidget` to manage the `PageView`.
    - Implemented a `PageView` with four colored containers as its children.
    - Initialized a `PageController` in `initState` to set the initial page of the `PageView` to the second page (index 1).

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,50 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(
+  MaterialApp(
+    home: HomePage(),
+  ),
+);
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
 
-  void _incrementCounter() {
-    setState(() {
+   late PageController _pageController;
 
-      _counter++;
-    });
-  }
+   @override
+  void initState(){
+
+     _pageController = PageController(
+       initialPage: 1,
+     );
+    super.initState();
+   }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+        body: PageView(
+            children: <Widget>[
+              Container(
+                color: Colors.blue,
+              ),
+              Container(
+                color: Colors.green,
+              ),
+              Container(
+                color: Colors.red,
+              ),Container(
+                color: Colors.white,
+              ),
+            ]
+        )
     );
   }
 }
